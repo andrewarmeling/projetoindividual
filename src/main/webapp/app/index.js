5373ecb1d72b451033e0b002ddaa88d096ac97f5
André Warmeling Gomes <andre.w.gomes@hotmail.com>
2019-06-25 09:39:17 -0300

    2019-06-24

--- src/main/webapp/app/index.js
+++ src/main/webapp/app/index.js
@@ -1,3 +1,15 @@
 $("#clientes").click(function() {
-    $("#page-content-wrapper").load("/cliente/index.html")
+    $("#page-content-wrapper").load("clientes")
+})
+
+$("#cadastrosAuxiliares").click(function() {
+    $("#page-content-wrapper").load("cadastrosauxiliares")
+})
+
+$("#buscarOS").click(function() {
+    $("#page-content-wrapper").load("buscaros")
+})
+
+$("#ordemDeServico").click(function() {
+    $("#page-content-wrapper").load("ordemdeservico")
 })
