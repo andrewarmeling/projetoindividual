63ceab4a208c8bd05e2bfbb1c2c43d91faf16568
André Warmeling Gomes <andre.w.gomes@hotmail.com>
2019-06-26 22:09:10 -0300

    2019-06-26

---
+++ target/projetoindividual/app/index.js
@@ -0,0 +1,15 @@
+$("#clientes").click(function() {
+    $(".conteudo").load("clientes")
+})
+
+$("#cadastrosAuxiliares").click(function() {
+    $(".conteudo").load("cadastrosauxiliares")
+})
+
+$("#buscarOS").click(function() {
+    $(".conteudo").load("buscaros")
+})
+
+$("#ordemDeServico").click(function() {
+    $(".conteudo").load("ordemdeservico")
+})
---
+++ target/projetoindividual/index.js
@@ -0,0 +1,20 @@
+function loginSubmit() {
+    var dados = {
+        login: $('#login').val(),
+        senha: btoa($('#senha').val())
+    };
+    
+    $.ajax({
+    	url: 'api/login',
+    	type: 'POST',
+    	data: JSON.stringify(dados),
+    	contentType: 'application/json; charset=utf-8',
+    	dataType: 'json',
+    	async: false,
+    	success: function(data) {
+    		window.location = data.url;
+    	}
+    });
+
+    return false;
+}
