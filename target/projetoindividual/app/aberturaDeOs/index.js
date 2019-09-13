17464baee6904b91a9255193314dabdd08a67060
André Warmeling Gomes <andre@andre-note.WORKGROUP>
2019-09-12 22:08:31 -0300

    2019-09-12

--- target/projetoindividual/app/aberturaDeOs/index.js
+++ target/projetoindividual/app/aberturaDeOs/index.js
@@ -1,3 +1,24 @@
+function abrirOS() {
+	if(validarCamposNecessarios()) {
+		$.ajax({
+			url: '../api/ordemDeServico/',
+			type: 'POST',
+			data: JSON.stringify(coletarDadosFormulario()),
+			contentType: 'application/json; charset="utf-8"',
+			dataType: 'json',
+			success: function (data) {
+				$('#OSAtual').val(data);
+				$('.conteudo').load("ordemdeservico");
+			},
+			error: function() {
+				console.log("erro ao cadastrar os");
+			}
+		});
+	} else {
+		console.log("erro ao abrir OS");
+	}
+}
+
 function exibirModalBuscaPorNome() {
 	$('#modal').load("clientes/modalBuscarCliente", function() {
 		$('#modal').modal();
@@ -36,7 +57,7 @@ function atualizarCampos(dados) {
 function validarCamposNecessarios() {
 	if ($('#id').val() != null) {
 		if (($('#serial').val().length > 3)) {
-			if (($('#solicitacao').val().lenght > 10)){
+			if (($('#solicitacao').val().length > 10)){
 				return true;
 			} else {
 				$('#solicitacao').focus();
@@ -49,13 +70,31 @@ function validarCamposNecessarios() {
 			return false;
 		}
 	} else {
-		$('#id').focus();
-		$('#id').select();
+		$('#cliente').focus();
+		$('#cliente').select();
 		return false;
 	};
 }
 
+function coletarDadosFormulario() {
+	var dados = {
+	idCliente: $('#id').val(),
+	tipoEquipamento: $('#tipo').val(),
+	marcaEquipamento: $('#marca').val(),
+	corEquipamento: $('#cor').val(),
+	modeloEquipamento: $('#modelo').val(),
+	serialEquipamento: $('#serial').val(),
+	solicitacao: $('#solicitacao').val(),
+	condicoesEquipamento: $('#condicoes').val(),
+	senhaEquipamento: $('#senha').val(),
+	observacoesInternas: $('#observacoes').val()
+	}
+	
+	return dados;
+}
+
 function limparFormulario() {
+	$('#id').val("");
 	$('#cliente').val("");
 	$('#tipo').val("");
 	$('#marca').val("");
@@ -66,6 +105,4 @@ function limparFormulario() {
 	$('#condicoes').val("");
 	$('#senha').val("");
 	$('#observacoes').val("");
-	
-//	mostrarBotoesFormularioLimpo();
 }
