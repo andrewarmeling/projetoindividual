42709f9b7ea1caa6b657a514f814979dbd8d99c6
André Warmeling Gomes <andre@andre-note.WORKGROUP>
2019-09-17 22:07:39 -0300

    2019-09-17

--- src/main/webapp/app/ordemdeservico/index.js
+++ src/main/webapp/app/ordemdeservico/index.js
@@ -4,6 +4,17 @@ $(document).ready(function(){
 	$('#prazo').datepicker({
 		uiLibrary: 'bootstrap4',
 		locale: 'pt-br',
-		format: 'dd/mm/yy'
+		format: 'dd/mm/yyyy'
 	});
+	$('#dataInicio').datetimepicker({
+		uiLibrary: 'bootstrap4',
+		locale: 'pt-br',
+		format: 'dd/mm/yyyy - HH:MM'
+	});
+	$('#dataFim').datetimepicker({
+		uiLibrary: 'bootstrap4',
+		locale: 'pt-br',
+		format: 'dd/mm/yyyy - HH:MM'
+	});
+
 });
